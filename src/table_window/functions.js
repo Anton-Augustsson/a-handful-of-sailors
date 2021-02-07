bccdc9a56347f6ef60e292571476cf65e3efbfbd
kotodox <isak98@live.se>
2021-02-07 16:25:06 +0100

    Created intial table window.

---
+++ src/table_window/functions.js
@@ -0,0 +1,26 @@
+function clickTable() {
+    window.location.href = "table_information.html"
+}
+/*
+var modal = document.getElementById("myModal");
+
+var span = document.getElementsByClassName("close")[0];
+
+var btn = document.getElementById("checkout");
+
+btn.onclick = function() {
+    modal.style.display = "block";
+    console.log("btn.onclick");
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+    console.log("span.onclick");
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+*/
