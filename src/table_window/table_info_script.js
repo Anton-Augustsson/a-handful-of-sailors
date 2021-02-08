547a723cb1c792c808c22a6568b7873aa359e269
Anton <Anton.Augustsson.4804@student.uu.se>
2021-02-08 16:53:26 +0100

    fixed issue with java script hade to be in code

---
+++ src/table_window/table_info_script.js
@@ -0,0 +1,23 @@
+
+window.onload = function(){
+  var modal = document.getElementById("myModal");
+  var span = document.getElementsByClassName("close")[0];
+  var btn = document.getElementById("checkout");
+  
+  btn.onclick = function() {
+    modal.style.display = "block";
+    console.log("btn.onclick");
+  }
+
+  span.onclick = function() {
+    modal.style.display = "none";
+    console.log("span.onclick");
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+  }
+}
+
