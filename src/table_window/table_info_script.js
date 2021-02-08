e65db17cdb0029383cef88737ac5a5aad6555fed
Anton <Anton.Augustsson.4804@student.uu.se>
2021-02-08 20:06:34 +0100

    css for table info

--- src/table_window/table_info_script.js
+++ src/table_window/table_info_script.js
@@ -2,11 +2,17 @@
 window.onload = function(){
   var modal = document.getElementById("myModal");
   var span = document.getElementsByClassName("close")[0];
-  var btn = document.getElementById("checkout");
-  
-  btn.onclick = function() {
+  var checkout = document.getElementById("checkout");
+  var cancel = document.getElementById("cancel");
+
+  checkout.onclick = function() {
     modal.style.display = "block";
-    console.log("btn.onclick");
+    console.log("checkout.onclick");
+  }
+
+  cancel.onclick = function() {
+    modal.style.display = "none";
+    console.log("cancel.onclick");
   }
 
   span.onclick = function() {
@@ -19,5 +25,10 @@ window.onload = function(){
         modal.style.display = "none";
     }
   }
+  //TODO: make a script that callse model for all added items in cart
 }
 
+function finish(){
+  window.location.href = "index.html";
+  //TODO: update model
+}
