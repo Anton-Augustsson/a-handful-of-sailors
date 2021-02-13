215d8d32da364972f1808a8243a08b37db09ec1a
Anton <Anton.Augustsson.4804@student.uu.se>
2021-02-13 17:05:42 +0100

    fixed generic color scheme and footer/header

--- src/table_window/table_info_script.js
+++ src/table_window/table_info_script.js
@@ -8,33 +8,26 @@ window.onload = function(){
   checkout.onclick = function() {
     modal.style.display = "block";
     console.log("checkout.onclick");
-  }
+  };
 
   cancel.onclick = function() {
     modal.style.display = "none";
     console.log("cancel.onclick");
-  }
+  };
 
   span.onclick = function() {
     modal.style.display = "none";
     console.log("span.onclick");
-  }
+  };
 
   window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-  }
+  };
   //TODO: make a script that callse model for all added items in cart
 
-  var footerElement = document.createElement('div')
-  var footer = document.getElementById("footer");
-  var footerContent = `
-    <button id="settings">settings</button>
-    <button id="login">login</button>`
-  footerElement.innerHTML = footerContent;
-  footer.append(footerElement);
-}
+};
 
 function finish(){
   window.location.href = "index.html";
