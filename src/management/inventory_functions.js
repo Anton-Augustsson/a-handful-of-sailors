f9eb3bef2625d5d125f4c2bc1f4877876f16a90e
kotodox <isak98@live.se>
2021-02-18 17:29:35 +0100

    "pushar lite faulty kod i hopp om att kunna få lite hjälp"

---
+++ src/management/inventory_functions.js
@@ -0,0 +1,16 @@
+
+function beverageList(){
+    var beveragelist = document.getElementById("beverages");
+    for (i = 0; i < DB2.spirits.length; i++) {
+        var name = DB2.spirits[i].namn;
+        var artikelid = DB2.spirits[i].artikelid;
+        var bevaregeContent = `
+            <div class="spirits" id="${name}" >
+                <span> Namn: ${name} Article Nr: ${artikelid} <span>
+            </div>`;
+        var beverageElement = document.createElement('div');
+        beverageElement.innerHTML= bevaregeContent;
+        beveragelist.appendChild(beverageElement);
+    }
+
+}
