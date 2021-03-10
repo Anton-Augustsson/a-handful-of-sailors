8c817134775b28ea80ff9baca35e3b510dbf22de
Anton <Anton.Augustsson.4804@student.uu.se>
2021-03-10 20:18:32 +0100

    fixed functionality and some basic style

--- src/vip/vip.js
+++ src/vip/vip.js
@@ -8,19 +8,29 @@
 
 function createVip(){
    return `
-    <button id="vip-payment" onclick=vipPayment()>pay</button>
-    <button id="vip-see-balance">see balance</button>
-    <div id="modal-vip-balance">
-    <div id="modal-content-vip-balance">
-        <span id="close-vip-balance">&times;</span>
-        <div id="vip-balance">100</div>
-        <form>
-            <label for="lamoutn">Amount:</label><br>
-            <input type="number" id="lamount" name="lamount" min="1" max="10000" value="1"><br><br>
-            <input type="submit" value="Submit" onclick=addBalance()>
-        </form>
-        <button id="cancel-vip-balance">close</button>
+    <div id="options-vip">
+        <button id="vip-payment" onclick=vipPayment()>pay</button>
+        <button id="vip-see-balance">see balance</button>
     </div>
+    <div id="modal-vip-balance">
+        <div id="modal-content-vip-balance">
+            <span id="close-vip-balance">&times;</span>
+            <div id="flex-vip-balance">
+                <div id="vip-balance-details">
+                    <div id="vip-balance-name"></div>
+                    <span id="vip-balance-text">Balance: </span>
+                    <span id="vip-balance"></span>
+                    <form>
+                        <label for="lamoutn">Amount to add: </label><br>
+                        <input type="number" id="lamount" name="lamount" min="0" max="10000" value="0"><br><br>
+                    </form>
+                </div>
+                <div id="options-vip-balance">
+                    <button onclick=addBalance()>Add amount</button>
+                    <button id="cancel-vip-balance">close</button>
+                </div>
+            </div>
+        </div>
     </div>
     `;
 }
@@ -38,7 +48,17 @@ function vipPayment(){
 }
 
 function addBalance(){
+    var amount = $("#lamount").val();
+    if(amount < 10000){
+        changeCapital(getItemUser(), amount);
+        setVipBalance();
+    }
+    else{
+        console.log("added to much");
+        alert("You have added to much");
+    }
 
+    $("#lamount").val('0');
 }
 
 function setViewBalance(){
@@ -71,6 +91,11 @@ function setViewBalance(){
 // =====================================================================================================
 // View update
 
+function setVipBalance(){
+    var details = getUserDetails(getItemUser());
+    $("#vip-balance").text(details.capital);
+    $("#vip-balance-name").text(details.firstName + " " + details.lastName);
+}
 
 function setVip(){
     $("#customer-vip").html(createVip());
@@ -93,6 +118,7 @@ function update_view_vip(){
 
     setVip();
     setViewBalance();
+    setVipBalance();
 }
 
 function init_vip(){
